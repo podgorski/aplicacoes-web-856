047eba049a96760dc63bd275117c6237a7fba53f
ANTÔNIO BEZERRA <11074166710@UC.ACAD>
2018-03-22 10:43:33 -0300

    outra alteração

--- js/core.js
+++ js/core.js
@@ -1,6 +1,8 @@
 var cesta_frutas = [];
 
-function adicionar_cesta(item){
+function adicionar_cesta(){
+    var item = document.getElementById("item").value;
+    //$("#id").val();
     cesta_frutas.push(item);
     alert(cesta_frutas);
 }
