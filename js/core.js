fe51c1b29a3b45bde345d25e778c621f22725350
ANTÔNIO BEZERRA <11074166710@UC.ACAD>
2018-03-29 10:13:25 -0300

    added jquery

--- js/core.js
+++ js/core.js
@@ -1,10 +1,20 @@
 var cesta_frutas = [];
 
 function adicionar_cesta(){
-    var item = document.getElementById("item").value;
-    //$("#id").val();
+    //var item = document.getElementById("item").value;
+    var item = $("#item").val();
     cesta_frutas.push(item);
-    alert(cesta_frutas);
+    //alert(cesta_frutas);
+    //var cesta = document.getElementById("cesta");
+    //cesta.innerHTML = cesta_frutas;
+    if (item === "x"){
+        $("#cesta").slideUp();
+    }else{
+        $("#cesta").append(item).slideDown(3000);
+    }
+    
+
+    
 }
 
 
